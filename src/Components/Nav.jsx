f42a073cbd72aadd3186352be87c93cf8e4b6692
ADITHYAN.A <113985941+adithya0808@users.noreply.github.com>
2024-03-06 19:50:24 +0530

    Update Nav.jsx

--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Nav = () => {
   return (
     <nav className='nav-bar' id='home'>
-        <div className="logo">Minons</div>
+        <div className="logo">Minions</div>
         <ul className="nav-links">
                 <li><a href="#home">Home</a></li>
                 <li><a href="#about">About</a></li>
