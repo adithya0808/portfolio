8d2c709ab4549bcff93faea70321432080d3b742
ADITHYAN.A <113985941+adithya0808@users.noreply.github.com>
2024-03-06 19:54:38 +0530

    Update Projects.jsx

--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -10,28 +10,28 @@ const Projects = () => {
                 <p className='para2'>Discover, Learn, Enjoy! 🌐🧠</p>
                 <p className='para3'>Engage in a quick and fun knowledge boost with our simple quiz app. Choose a category, answer questions, and challenge your intellect effortlessly. </p>
                 <p className="para4">HTML,CSS,React.js</p>
-                <a href="" className='git1'>Click here</a>
+                <a href="https://fantastic-florentine-683006.netlify.app/" className='git1'>Click here</a>
             </div>
             <div className="card">
                 <p className='text1'>Fitness Tracking Website</p>
-                <p className='para2'>Discover, Learn, Enjoy! 🌐🧠</p>
+                <p className='para2'>Discover, Learn, Enjoy! 💪🌟</p>
                 <p className='para3'>Engage in a quick and fun knowledge boost with our simple quiz app. Choose a category, answer questions, and challenge your intellect effortlessly. </p>
                 <p className="para4">HTML,CSS,React.js</p>
-                <a href="" className='git1'>Click here</a>
+                <a href="/home" className='git1'>Soon</a>
             </div>
             <div className="card">
-                <p className='text1'>Restaurant site</p>
-                <p className='para2'>Discover, Learn, Enjoy! 🌐🧠</p>
-                <p className='para3'>Engage in a quick and fun knowledge boost with our simple quiz app. Choose a category, answer questions, and challenge your intellect effortlessly. </p>
+                <p className='text1'>Discount calculator</p>
+                <p className='para2'>Discover, Learn, Enjoy! 💰🎉</p>
+                <p className='para3'>Get knowledge discount with our discount calculator. Enter the Amount and Discount Percentage. Then calculate actual discount. </p>
                 <p className="para4">HTML,CSS,React.js</p>
-                <a href="" className='git1'>Click here</a>
+                <a href="https://discountcheck1.netlify.app/" className='git1'>Click here</a>
             </div>
             <div className="card">
-                <p className='text1'>Clone of Spotify</p>
-                <p className='para2'>Discover, Learn, Enjoy! 🌐🧠</p>
-                <p className='para3'>Engage in a quick and fun knowledge boost with our simple quiz app. Choose a category, answer questions, and challenge your intellect effortlessly. </p>
+                <p className='text1'>Magazine App</p>
+                <p className='para2'>Discover, Learn, Enjoy! 📖🌟</p>
+                <p className='para3'>Engage in a quick and knowledge boost with our Magazine app. Choose a category, read the magazine gain knowledge. </p>
                 <p className="para4">HTML,CSS,React.js</p>
-                <a href="" className='git1'>Click here</a>
+                <a href="" className='git1'>Git repo</a>
             </div>
         </div>
     </div>
