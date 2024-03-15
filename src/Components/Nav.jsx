b4221db47609b37f712811abf3cb023bfa14ff3b
My Name <myemail@example.com>
2024-03-15 11:07:25 +0530

    tr-one

--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -1,15 +1,16 @@
 import React from 'react'
-
+import bar from '../assets/bar.png'
 const Nav = () => {
   return (
     <nav className='nav-bar' id='home'>
-        <div className="logo">Minons</div>
+        <div className="logo">Minions</div>
         <ul className="nav-links">
                 <li><a href="#home">Home</a></li>
                 <li><a href="#about">About</a></li>
                 <li><a href="#projects">Projects</a></li>
                 <li><a href="#contact">Contact</a></li>
         </ul>
+        <img src={bar} alt="none" className="bar" />
     </nav>
   )
 }
